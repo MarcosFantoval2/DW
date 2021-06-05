03c6dc1d76183af0b162ec74932950603a7f2fd9
MarcosFantoval2 <40368903+MarcosFantoval2@users.noreply.github.com>
2021-06-04 20:12:07 -0400

    primera prueba php vercion 1.1

---
+++ checklogin.php
@@ -0,0 +1,14 @@
+<?php
+$usuarios = Array("hola@hola.com" => 'hola', "hola2@hola.com" => '1234');
+$esta=false;
+foreach($usuarios as $email =>$pass){
+    if ($email == $_POST['email'] && $pass == $_POST['password']){
+        $esta = true;
+    }
+}
+if ($esta){
+    header("Location:perfil.html");
+}else{
+    header("Location:iniciar.html?error=1");
+}
+?>
