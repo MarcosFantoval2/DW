6bffc14bdaf9756e4332abfc29bd008d452e16ec
MarcosFantoval2 <40368903+MarcosFantoval2@users.noreply.github.com>
2021-06-04 20:54:02 -0400

    prueba subir foto php vercion 1.2

---
+++ cregistration.php
@@ -0,0 +1,21 @@
+<?php 
+$target_dir = "images";
+$target_file = $target_dir.uniqid() .".png";
+$uploadOK=1;
+$check=getimagesize($_FILES["fotoPerfil"]["tmp_name"]);
+
+if ($check!==false){
+    $uploadOK=1;
+}   
+else{
+    $uploadOK=0;
+    die("Error: el archivo no es una foto");
+}
+
+if (move_uploaded_file($_FILES["fotoPerfil"]["tmp_name"], $target_file)){
+    echo "El archivo". htmlspecialchars(basename( $_FILES["fileToUpload"]["name"]))."fue cargado.";
+}
+else{
+    echo"Problema en cargar el archivo";
+}
+?>
