c6858595c119ecfe180e5a156371b6516fc63f4a
MarcosFantoval2 <40368903+MarcosFantoval2@users.noreply.github.com>
2021-06-06 23:02:46 -0400

    prueba de registro version 1.4.18

--- registroimagen.php
+++ registroimagen.php
@@ -59,19 +59,19 @@
         
         <form action= "cregistration.php" method="POST" enctype="multipart/form-data" >
                 <div class="mb-3">
-                    <input type="hidden"  id="Name" name="Name" value= "<?php echo($_POST["nombre"])?>">
+                    <input type="hidden"  id="Name" name="nombre" value= "<?php echo($_POST["nombre"])?>">
                 </div>
                 <div class="mb-3">
-                    <input type="hidden"  id="unive" name= "unive" value= "<?php echo($_POST["uni"])?>" >
+                    <input type="hidden"  id="unive" name= "uni" value= "<?php echo($_POST["uni"])?>" >
                 </div>
                 <div class="mb-3">    
-                    <input type="hidden"  id="Date" name="Date" value= "<?php echo($_POST["fecha"])?>">
+                    <input type="hidden"  id="Date" name="fecha" value= "<?php echo($_POST["fecha"])?>">
                 </div>
                 <div class="mb-3">
-                <input type="hidden"  id="Email1" value= "<?php echo($_POST["email"])?>">
+                <input type="hidden"  id="Email1" name ="email"value= "<?php echo($_POST["email"])?>">
                 </div>
                 <div class="mb-3">
-                <input type="hidden"  id="Password1"value= "<?php echo($_POST["contraseña"])?>" >
+                <input type="hidden"  id="Password1"name ="contraseña"value= "<?php echo($_POST["contraseña"])?>" >
                 </div>
                 <input type="file" name="fotoperfil" id="fotoperfil" accept="image/png, image/jpg"/>
                 <input type="submit" name="subir-imagen" value="Enviar imagen" />
