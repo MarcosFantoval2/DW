1bf5c459b59e536c261a4c01d4a90639d2f10bf7
MarcosFantoval2 <40368903+MarcosFantoval2@users.noreply.github.com>
2021-06-06 21:16:34 -0400

    prueba de registro de imagen version 1.4.8

--- cregistration.php
+++ cregistration.php
@@ -2,7 +2,7 @@
 $target_dir = "images";
 $target_file = $target_dir.uniqid() .".png";
 $uploadOK=1;
-$check=getimagesize($_FILES["fotoPerfil"]["tmp_name"]);
+$check=getimagesize($_FILES["fotoperfil"]["tmp_name"]);
 
 if ($check!=false){
     $uploadOK=1;
@@ -12,7 +12,7 @@ else{
     die("Error: el archivo no es una foto");
 }
 
-if (move_uploaded_file($_FILES["fotoPerfil"]["tmp_name"], $target_file)){
+if (move_uploaded_file($_FILES["fotoperfil"]["tmp_name"], $target_file)){
     echo "El archivo". htmlspecialchars(basename( $_FILES["fileToUpload"]["name"]))."fue cargado.";
 }
 else{
