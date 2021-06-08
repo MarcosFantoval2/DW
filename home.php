1dc75bd544e5b916960b9c432afcdd65381d92e0
MarcosFantoval2 <40368903+MarcosFantoval2@users.noreply.github.com>
2021-06-07 21:40:23 -0400

    prueba de imagen y cambio de orden 1.8.5

--- home.php
+++ home.php
@@ -82,7 +82,7 @@ foreach($posts as $post){
 <div id="" class="container-md border border-primary mt-3" style="padding: 10;">
     <div class="row">
         <div class="col-sm-auto"> 
-            <img src="<?php echo($usuarios->findOne(["nombre"=>$post->username]))?>" height="150px">
+            <img src="<?php echo($user->img); ?>" height="150px">
         </div>
         <div class="col">
             <div class="row">
