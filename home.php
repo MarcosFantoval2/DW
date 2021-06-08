c565e3e6c8f7bcd98b95e1a02b61a74c1cef0dfe
MarcosFantoval2 <40368903+MarcosFantoval2@users.noreply.github.com>
2021-06-07 20:32:16 -0400

    prueba prueba de publicacion 1.7.8

--- home.php
+++ home.php
@@ -90,7 +90,7 @@ foreach($posts as $post){
                 <br>
             </div>
             <div class="row">
-                <p class="font-weight-bold"><?php echo($post["texto"])?></p>
+                <p class="font-weight-bold"><?php echo($post->texto)?></p>
             </div>
         </div>
         <div class="row">
